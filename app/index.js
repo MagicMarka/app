6816effe16c2e0e06a6001ba1fd605d1d992dea3
unknown <magicformarka@gmail.com>
2017-11-20 17:33:33 +0200

    first commit

---
+++ app/index.js
@@ -0,0 +1,13 @@
+require('bootstrap/dist/css/bootstrap.min.css');
+require('./css/main.css');
+import React from 'react';
+import ReactDOM from 'react-dom';
+export default class HelloWorld extends React.Component {
+  render() {
+    return(<input type="button" className="btn btn-primary" value="Hello World" />);
+  }
+}
+ReactDOM.render(
+  <HelloWorld />,
+  document.getElementById('app')
+);
---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+const path = require('path');
+const PATHS = {
+       app: path.join(__dirname,'app'),
+       build: path.join(__dirname,'build')
+     };
+module.exports = {
+       entry: {
+              app: PATHS.app
+       },
+       output: {
+         path: PATHS.build,
+         filename: 'bundle.js'
+       },
+       module: {
+     loaders: [
+      {
+          test: /\.css$/,
+          loaders: ['style-loader','css-loader']
+      },
+      {
+         test: /\.js?/,
+         loader: 'babel-loader'
+      },
+      {
+         test: /\.png$/,
+        loader: "url-loader?limit=100000"
+      },
+      {
+        test: /\.jpg$/,
+        loader: "file-loader"
+      },
+      {
+       test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'url-loader? limit=10000&mimetype=application/font-woff'
+       },
+       {
+        test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'url-loader?limit=10000&mimetype=application/octet-stream'
+       },
+       {
+        test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'file-loader'
+        },
+        {
+        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'url-loader?limit=10000&mimetype=image/svg+xml'
+       }
+     ]
+    },
+ devServer: {
+    host: '127.0.0.1',
+    port: 4040,
+  }
+     };
