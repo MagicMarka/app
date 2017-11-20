ff224ed05fde8da9c1e654776e5eb53ead9f60e5
unknown <magicformarka@gmail.com>
2017-11-20 22:53:56 +0200

    styles added

--- app/index.js
+++ app/index.js
@@ -1,7 +1,12 @@
 require('bootstrap/dist/css/bootstrap.min.css');
 require('./css/main.css');
+import 'font-awesome/css/font-awesome.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './css/main.css';
+import './css/style.css';
+import './css/theme.css';
+import './css/jsgrid.css';
 export default class HelloWorld extends React.Component {
   render() {
     return(<input type="button" className="btn btn-primary" value="Hello World" />);
