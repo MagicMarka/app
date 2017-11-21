a7a6d58e35777d32ebd1d718b43dd9627d4c4f24
unknown <magicformarka@gmail.com>
2017-11-21 19:54:31 +0200

    clean style

--- app/index.js
+++ app/index.js
@@ -1,17 +1,12 @@
 require('bootstrap/dist/css/bootstrap.min.css');
 require("jquery");
 require('./css/style.css');
-require('./css/theme.css');
-require('./css/jsgrid.css');
-
-
 
 import React from 'react';
 import ReactDOM from 'react-dom';
 import $ from "jquery";
 import './css/style.css';
-import './css/theme.css';
-import './css/jsgrid.css';
+
 
 
 export default class HelloWorld extends React.Component {
